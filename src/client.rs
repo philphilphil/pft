cb7b85c389f8eba04027cd5145e7ce5a9267c12c
Phil Baum <philipp.baum@mailbox.org>
2022-05-30 21:57:14 +0200

    first working file transfer :O

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,8 @@
-use std::{io::Write, net::TcpStream};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    net::TcpStream,
+};
 
 use crate::Request;
 
@@ -7,9 +11,22 @@ pub fn start() {
         Ok(mut stream) => {
             println!("Connection successfull on port 3030");
 
-            let req = Request::Echo("does this really still work?".to_string());
-            let _serialized = req.serialize(&mut stream);
-            stream.flush().unwrap();
+            // let req = Request::Echo("does this really still work?".to_string());
+            // let _serialized = req.serialize(&mut stream);
+            // stream.flush().unwrap();
+            let mut file = File::open("testfile.txt").unwrap();
+
+            let mut buf = [0; 4096];
+            loop {
+                let n = file.read(&mut buf).unwrap();
+
+                if n == 0 {
+                    // reached end of file
+                    break;
+                }
+
+                stream.write_all(&buf[..n]).unwrap();
+            }
         }
         Err(e) => {
             println!("Failed to connect: {}", e);
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{fs::File, io, net::TcpListener};
 
 use crate::Request;
 
@@ -6,10 +6,12 @@ pub fn start() {
     let listener = TcpListener::bind("localhost:3030").unwrap();
 
     for mut stream in listener.incoming().flatten() {
-        let request = Request::deserialize(&mut stream).unwrap();
-        match request {
-            Request::Echo(msg) => println!("{}", msg),
-            Request::Jumble { .. } => todo!(),
-        }
+        let mut file = File::create("downloadedfiles.txt").unwrap();
+        io::copy(&mut stream, &mut file).unwrap();
+        // let request = Request::deserialize(&mut stream).unwrap();
+        // match request {
+        //     Request::Echo(msg) => println!("{}", msg),
+        //     Request::Jumble { .. } => todo!(),
+        // }
     }
 }
