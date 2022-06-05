d0bf32b23bbf4c3d9dc32bb6faf591f46b44ef7f
Phil Baum <philipp.baum@mailbox.org>
2022-06-05 08:54:09 +0200

    save before command restructure

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,28 @@
 use std::net::SocketAddr;
 
-use clap::{ArgEnum, ArgGroup, Parser};
+use clap::{ArgEnum, ArgGroup, Parser, Subcommand};
 use pft::{client, server};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
-#[clap(group(
-            ArgGroup::new("client")
-                .required(false)
-                .args(&["mode", "host", "port", "otp"]),
-        ))]
 struct Args {
-    /// Mode to run as
-    #[clap(arg_enum)]
-    mode: Mode,
+    #[clap(subcommand)]
+    command: Commands,
+    // /// Mode to run as
+    // #[clap(arg_enum)]
+    // mode: Mode,
 
-    /// Port to listen or sent to
-    #[clap(short, default_value = "localhost")]
-    host: String,
+    // /// Port to listen or sent to
+    // #[clap(short, default_value = "localhost")]
+    // host: String,
 
-    /// Port to listen or sent to
-    #[clap(short, long, long,parse(try_from_str=parse_port_input),default_value_t = 3030)]
-    port: usize,
+    // /// Port to listen or sent to
+    // #[clap(short, long, long,parse(try_from_str=parse_port_input),default_value_t = 3030)]
+    // port: usize,
 
-    /// One-time password for authentication
-    #[clap(short, long)]
-    otp: String,
+    // /// One-time password for authentication
+    // #[clap(short, long)]
+    // otp: String,
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
@@ -34,14 +31,29 @@ enum Mode {
     Client,
 }
 
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[clap(arg_required_else_help = true)]
+    Clone {
+        /// The remote to clone
+        remote: String,
+    },
+    /// pushes things
+    #[clap(arg_required_else_help = true)]
+    Push {
+        /// The remote to target
+        remote: String,
+    },
+}
+
 fn main() {
     let args = Args::parse();
     let address = SocketAddr::from(([127, 0, 0, 1], 3030));
 
-    match args.mode {
-        Mode::Server => server::start(&address),
-        Mode::Client => client::start(&address),
-    }
+    // match args.mode {
+    //     Mode::Server => server::start(&address),
+    //     Mode::Client => client::start(&address),
+    // }
 }
 
 fn parse_port_input(input_port: &str) -> Result<usize, String> {
