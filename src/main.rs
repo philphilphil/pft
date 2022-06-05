a802b122166c01a26e029dbf620a9687ba30d28c
Phil Baum <philipp.baum@mailbox.org>
2022-06-05 08:52:12 +0200

    save before command restructure

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 use std::net::SocketAddr;
 
-use clap::{ArgEnum, Parser};
+use clap::{ArgEnum, ArgGroup, Parser};
 use pft::{client, server};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
+#[clap(group(
+            ArgGroup::new("client")
+                .required(false)
+                .args(&["mode", "host", "port", "otp"]),
+        ))]
 struct Args {
     /// Mode to run as
     #[clap(arg_enum)]
     mode: Mode,
 
     /// Port to listen or sent to
-    #[clap(short, long, default_value_t = 3030)]
-    port: u16,
+    #[clap(short, default_value = "localhost")]
+    host: String,
+
+    /// Port to listen or sent to
+    #[clap(short, long, long,parse(try_from_str=parse_port_input),default_value_t = 3030)]
+    port: usize,
+
+    /// One-time password for authentication
+    #[clap(short, long)]
+    otp: String,
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
@@ -29,4 +42,16 @@ fn main() {
         Mode::Server => server::start(&address),
         Mode::Client => client::start(&address),
     }
+}
+
+fn parse_port_input(input_port: &str) -> Result<usize, String> {
+    let port: usize = input_port
+        .parse()
+        .map_err(|_| format!("{} is not a valid number.", input_port))?;
+
+    if !(1..=65535).contains(&port) {
+        return Err(format!("{} is not a valid port.", port));
+    }
+
+    Ok(port)
 }
